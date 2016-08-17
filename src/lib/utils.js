f2e20897afc58d5d5b6f7b88e6bcfcc574d16420
Jorge Padilla <jpadilla@us.ibm.com>
2016-08-17 16:06:44 -0400

    Refactor code to solve various problems
    - Resolve #46.
    - Make saving feedback data to db independent of bot processing so
    failures with the db operation don’t cause the bot processing to stop.
    - Emit to ibmcloud.formatter instead of using res.reply
    - Add more logging statements and improve existing ones.

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -8,6 +8,24 @@
 
 const constants = require('./constants');
 const env = require('./env');
+const nlcDb = require('hubot-ibmcloud-cognitive-lib').nlcDb;
+
+// --------------------------------------------------------------
+// i18n (internationalization)
+// It will read from a peer messages.json file.  Later, these
+// messages can be referenced throughout the module.
+// --------------------------------------------------------------
+const i18n = new (require('i18n-2'))({
+	locales: ['en'],
+	extension: '.json',
+	// Add more languages to the list of locales when the files are created.
+	directory: __dirname + '/../messages',
+	defaultLocale: 'en',
+	// Prevent messages file from being overwritten in error conditions (like poor JSON).
+	updateFiles: false
+});
+// At some point we need to toggle this setting based on some user input.
+i18n.setLocale('en');
 
 function logMessage(robot, res, userId, text, tgt) {
 	let key = userId + constants.LOGGER_KEY_SUFFIX;
@@ -32,7 +50,8 @@ function logMessage(robot, res, userId, text, tgt) {
 			if (info.messagesToSave === 0){
 				// remove original classified statement
 				info.logs = info.logs.slice(1);
-				robot.emit('nlc.feedback.negative.js', res, info);
+				// robot.emit('nlc.feedback.negative.js', res, info);
+				handleFeedback(robot, res, info);
 				info = null;
 			}
 		}
@@ -48,6 +67,35 @@ function logMessage(robot, res, userId, text, tgt) {
 	robot.brain.set(key, info);
 };
 
+
+function handleFeedback(robot, res, info){
+	nlcDb.open().then((db) => {
+		if (info && info.id){
+			// low / med event log path
+			// save additional input texts to original 'target' document
+			db.get(info.id).then((doc) => {
+				doc.logs = info.logs;
+				return db.put(doc);
+			}).catch((err) => {
+				robot.logger.error(err);
+			});
+		}
+		else {
+			// get user logs from the brain
+			let userId = res.envelope.user.id;
+			let key = userId + constants.LOGGER_KEY_SUFFIX;
+			let info = robot.brain.get(key);
+			if (info && info.logs){
+				return db.post(info.logs, 'negative_fb');
+			}
+		}
+	}).catch((err) => {
+		robot.logger.error(err);
+	});
+};
+
+
 module.exports = {
-	logMessage: logMessage
+	logMessage: logMessage,
+	handleFeedback: handleFeedback
 };
